961197c93bb63220a49c826d3eca8bb9983e89d2
ghoshbinayak <ghoshbinayak@gmail.com>
2013-10-31 03:18:53 +0530

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+// Project configuration.
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        my_targer: {
+            files: {
+                'dist/js/main.min.js': 'src/js/main.js',
+                'dist/js/plugins.min.js': 'src/js/plugins.js'
+            }
+        }
+    },
+
+    jshint: {
+        options: {
+            curly: true,
+            eqeqeq: true,
+            eqnull: true,
+            globals: {
+                jQuery: true
+            },
+        },
+        files: {
+            src: ['src/js/main.js', 'src/js/plugins.js']
+        }
+    },
+
+    cssmin: {
+        combine: {
+            files: {
+                'dist/css/main.min.css': ['src/css/normalize.css', 'src/css/main.css']
+            }
+        },
+        add_banner: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            files: {
+                'dist/css/main.min.css': ['dist/css/main.min.css']
+            }
+        }
+    },
+    watch: {
+        options: {
+            livereload: true
+        },
+        src: {
+          files: ['src/css/*.js', 'src/css/*.css', '*.html'],
+          tasks: ['default']
+        }
+    }
+});
+
+// Load the packages for performing the tasks
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+// Default task(s).
+grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+grunt.registerTask('livewatch', ['watch']);
+
+};
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+var INS = {
+  updates: 'no recent updates',
+  log: function(msg) {
+    console.log('instruo: ' + msg);
+  },
+};
+
+INS.log(INS.updates);
